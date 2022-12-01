5660d1f0585c1396129626d831a5e6ebb0477a8d
Juliane <julianesilvac75@gmail.com>
2022-12-01 13:53:51 +0000

    feat: create scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,8 +38,28 @@ def scrape_next_page_link(html_content: str):
 
 
 # Requisito 4
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+def scrape_noticia(html_content: str):
+    selector = Selector(html_content)
+
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css("h1.entry-title::text").get().rstrip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    comments_count = len(selector.css("ol.comment-list li").getall())
+    summary = selector.xpath("string(//p)").get().rstrip()
+    tags = selector.css("section.post-tags a::text").getall()
+    category = selector.css("a.category-style span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
