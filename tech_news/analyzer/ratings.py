7f3650702d48b4a0fb62d658881359667bfd0d0c
Juliane <julianesilvac75@gmail.com>
2022-12-05 15:29:29 +0000

    feat: implements top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = search_news({
+        "$query": {},
+        "$orderby": {"comments_count": -1, "title": 1}
+    })
+
+    return [(new["title"], new["url"]) for new in news[:5]]
 
 
 # Requisito 11
